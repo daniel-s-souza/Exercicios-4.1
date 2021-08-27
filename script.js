063957acc24ba436a21e2dd927d28514238bf69b
Daniel <danielsantsouza94@gmail.com>
2021-08-27 17:56:49 -0300

    exercicios java script

---
+++ script.js
@@ -0,0 +1,85 @@
+// let taskList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// console.log(taskList.length);
+
+// let taskList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// let searchForFirstTask = taskList[0];
+// console.log(searchForFirstTask)
+
+// let taskList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// let searchForFirstTask = taskList[taskList.length - 1];
+// console.log(searchForFirstTask)
+
+// let taskList = ['Tomar café', 'Reunião', 'Brincar com o cachorro']
+// taskList.push('Fazer exercícios da Trybe');
+// taskList.unshift('Levantar da cama');
+// console.log(taskList)
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// tasksList.pop();
+// tasksList.shift();
+// console.log(tasksList)
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+// let indexOfTask = tasksList.indexOf('Reunião')
+// console.log(indexOfTask)
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let menuServices = menu[1] ;
+
+// console.log(menuServices);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// let indexOfPortfolio = menu.indexOf('Portfólio');
+
+// console.log(indexOfPortfolio);
+
+// let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+// menu.push('Contato')
+
+// console.log(menu);
+
+// let numero = 7;
+
+// for(let contador =0;contador <= 10;contador++){
+//   console.log (numero*contador);
+// }
+
+// let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+// // groceryList.push('Carne')
+
+// for(let indice =0;indice < groceryList.length;indice += 1){
+//   console.log(groceryList[indice])
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// for(let name of names){
+//   console.log(name)
+// }
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// console.log(numbers)
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let sum = 0;
+// for(let i =0; i < numbers.length; i += 1){
+//   sum += numbers[i]
+//   console.log(sum)
+// }
+
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let media = (numbers[0] + numbers[1] + numbers[2] + numbers[3] + numbers[4] + numbers [5] + numbers [6] + numbers[7] + numbers[8] + numbers[9])/10;
+
+// if(media>20){
+//   console.log("Valor maior que 20");
+// }else{
+//  console.log("Valor menor ou igual a 20")
+// }
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// let maiorNumero = Math.max.apply(null, numbers);
+// for(let i = 0; i < numbers.length; i += 1){
+//   if(numbers[i] === maiorNumero){
+//     console.log (numbers[i]);
+//   }
+// }
